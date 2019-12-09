604e22665b1f902f12b6b99d8ab6b64465e96e04
bvenkatr <bvenkatr2@gmail.com>
2019-12-09 22:21:21 +0530

    add dotenv library to load environment variables

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module dropbox-utility
 
 go 1.13
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/tj/go-dropbox v0.0.0-20171107035848-42dd2be3662d
+	github.com/tj/go-dropy v0.0.0-20151223190506-225699a12156
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/tj/go-dropbox"
+	"github.com/tj/go-dropy"
+)
 
 func main() {
-	fmt.Println("Dropbox utility")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	client := dropy.New(dropbox.New(dropbox.NewConfig(os.Getenv("DROPBOX_ACCESS_TOKEN"))))
+	filesList, err := client.ListN("", 2)
+	if err != nil {
+		fmt.Println("Got error while listing the files ", err)
+	}
+
+	fmt.Println(len(filesList))
+
+	fileNamesList := []string{}
+	for _, fileInfo := range filesList {
+		fileNamesList = append(fileNamesList, fileInfo.Name())
+	}
+	fmt.Println("List of file name are", fileNamesList)
 }
