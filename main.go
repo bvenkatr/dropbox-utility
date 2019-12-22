cb082b1556cc57b4480c8c2310830f2e8c7d77a7
bvenkatr <bvenkatr2@gmail.com>
2019-12-22 22:19:45 +0530

    interim

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox"
-	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox/users"
+	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox/files"
 	"github.com/joho/godotenv"
 )
 
@@ -15,11 +15,44 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	/*
+		,
+			LogLevel: dropbox.LogInfo, // if needed, set the desired logging level. Default is off
+	*/
 	config := dropbox.Config{
-		Token:    os.Getenv("DROPBOX_ACCESS_TOKEN"),
-		LogLevel: dropbox.LogInfo, // if needed, set the desired logging level. Default is off
+		Token: os.Getenv("DROPBOX_ACCESS_TOKEN"),
 	}
-	dbx := users.New(config)
+	dbx := files.New(config)
+	// s := new(files.ListFolderResult)
+
 	// start making API calls
-	fmt.Println(dbx)
+	res, err := dbx.ListFolder(files.NewListFolderArg(""))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var entries []files.IsMetadata
+	entries = res.Entries
+
+	for i, entry := range entries {
+		switch f := entry.(type) {
+		case *files.FileMetadata:
+			printFileMetadata(f)
+		case *files.FolderMetadata:
+			printFolderMetadata(f)
+		}
+		if i%4 == 0 {
+			fmt.Println("i%4 == 0...., nothing ")
+		}
+	}
+}
+
+func printFolderMetadata(e *files.FolderMetadata) {
+	fmt.Println("*files.FileMetadata....")
+	fmt.Printf("%s\t\n", e.PathDisplay)
+}
+
+func printFileMetadata(e *files.FileMetadata) {
+	fmt.Println("*files.FolderMetadata....")
+	fmt.Printf("%s\t", e.PathDisplay)
 }
